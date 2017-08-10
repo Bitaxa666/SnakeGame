1f316f1b59cd58770d4cb1e641056d0833ed8515
user <repcity@yandex.ru>
2017-08-10 14:40:38 +0300

    Commit the Snack game on Java. Have one level

---
+++ src/MainWindow.java
@@ -0,0 +1,24 @@
+import javax.swing.*;
+
+/**
+ * Created by user on 8/2/17.
+ */
+public class MainWindow extends JFrame
+{
+    public MainWindow()
+    {
+        setTitle("My Game");
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setSize(320, 345);
+        setLocation(400,400);
+        add(new GameField());
+        setVisible(true);
+
+    }
+
+    public static void main(String[] args)
+    {
+        MainWindow newWindow = new MainWindow();
+
+    }
+}
